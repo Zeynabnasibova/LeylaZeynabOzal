2096b5df31925274f6843cfed6f8337c9721d2fa
Ozal14 <lider.ovm@gmail.com>
2021-10-19 12:10:16 -0400

    Inner Outer Array

---
+++ src/practice/arrayInnerOuter.java
@@ -0,0 +1,47 @@
+package practice;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class arrayInnerOuter {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int sizeInner=scan.nextInt();
+        int sizeOuter=scan.nextInt();
+
+        int[] inner = new int[scan.nextInt()];
+        int[] outer = new int[scan.nextInt()];
+
+        for(int i = 0; i <sizeInner; i++) {
+            inner[i] = scan.nextInt();
+        }
+        for(int j = 0; j < sizeOuter; j++) {
+            outer[j] = scan.nextInt();
+        }
+
+        Arrays.sort(inner);
+        Arrays.sort(outer);
+
+        //WRITE YOUR CODE HERE
+
+
+        int count =0;
+        for (int innerEach : inner){
+
+            for (int outerEach : outer) {
+
+                if( innerEach == outerEach){
+                    count++;
+
+                }
+            }
+
+
+        }
+        System.out.println (count == sizeOuter);
+
+
+
+    }
+    }
+
